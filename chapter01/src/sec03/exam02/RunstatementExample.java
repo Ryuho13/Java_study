e15e1e378b0b850c16137d9bba7ab1b7599f2ae0
ryho <hosedk99@gamil.com>
2025-07-15 10:14:26 +0900

    초기프로

---
+++ chapter01/src/sec03/exam02/RunstatementExample.java
@@ -0,0 +1,14 @@
+package sec03.exam02;
+
+public class RunstatementExample {
+
+	public static void main(String[] args) {
+		int x = 1; // 변수 x를 선언하구 1을 저장
+		int y = 2; // 변수 y를 선언하구 2을 저장
+		int result = x + y; // 변수 result를 선언하구 x와 y룰 더한 값을 저장
+		System.out.println(result); // println 메소드 호술 => 콘솔에 출력
+	}
+		
+}
+
+
